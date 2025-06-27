b4b4d63d088f256493ac15bc9aa0f611c6ebb881
siammehraf <siammehrafworks@gmail.com>
2025-06-27 06:12:31 +0600

    Some Basic Changes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,38 @@
 import Navbar from '@/components/Navbar';
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam aliquam numquam est deleniti, accusamus rerum
+        voluptatibus, a recusandae veniam doloremque totam non cupiditate? Odio molestias nulla vero porro tempore est?
+        Neque ullam reprehenderit placeat ex dolores distinctio molestias quod voluptatum alias eligendi at, possimus
+        inventore debitis labore doloribus enim magni maiores nobis perferendis amet minima. Magni omnis voluptatem a
+        corrupti! In veritatis maxime libero omnis blanditiis repudiandae tenetur dolor dignissimos eligendi. Aspernatur
+        quae assumenda ipsa labore delectus excepturi, hic tenetur dolore? Exercitationem unde ducimus recusandae sunt
+        officia facilis officiis accusantium. Maiores earum, magni odio vero quo ab harum nemo minima labore cupiditate.
+        Repudiandae repellendus porro soluta hic, quis minus odio neque nemo cum similique accusantium deleniti minima
+        officia dolorem numquam. Minus esse eum ducimus nisi temporibus nam excepturi tempore quidem odio reiciendis
+        earum et illum doloremque modi, facere placeat. Veniam iure dicta distinctio excepturi rem nesciunt aspernatur
+        voluptatem dolore necessitatibus! Sint fugit doloremque harum voluptates laboriosam aspernatur, dolorum tempora
+        obcaecati dolore autem enim consequatur quis incidunt nostrum ducimus accusantium minus velit? Eaque cum nulla
+        animi rerum saepe, pariatur mollitia ut? Maxime officiis consequuntur, illum quibusdam ea eveniet sequi nemo
+        facilis eaque vero omnis, sapiente animi temporibus aspernatur distinctio. Ab, aperiam omnis. Incidunt est
+        laborum necessitatibus in nulla. Earum, atque id? Voluptate enim molestias doloremque odit modi! Et, unde quo
+        distinctio ut omnis tempora id possimus nulla, iusto reprehenderit aliquid laudantium eligendi, nemo sapiente
+        molestias fuga quibusdam minus aspernatur error. Nisi! Quaerat, rem deserunt, incidunt, voluptates corporis
+        consequuntur totam ea iusto impedit quod blanditiis fugit ex excepturi tempora distinctio nam architecto non
+        cumque? Consequatur voluptatibus qui, temporibus eius eveniet accusantium autem? Nulla, nostrum ut architecto
+        veniam omnis quos rem sequi, fugit laborum iusto facilis similique sunt libero aliquid perferendis? Magnam porro
+        repellat quisquam dolorem! Quis sequi nam natus deserunt voluptatem distinctio! Ea distinctio quo quasi dolor
+        earum perspiciatis voluptatum inventore suscipit, praesentium iure nihil saepe blanditiis illum officia ex
+        consectetur illo excepturi, tempore laboriosam placeat ratione? Magnam voluptatem molestiae voluptate magni?
+        Illu modi rem assumenda ea recusandae dicta nobis sequi, dolores et veniam voluptates necessitatibus reiciendis
+        dolor nesciunt fugiat exercitationem provident, sit corporis porro! Ad eos dolorum reiciendis. Laborum est
+        cumque explicabo enim. Dolore laudantium a eaque ut, rem architecto tempore, iure atque nobis tenetur rerum, vel
+        facilis quam quae maxime nam? Ab similique doloribus dolores amet autem. Aliquid doloribus saepe accusamus hic
+        libero ab, rerum at iste maio
+      </p>
+    </main>
+  );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -80,7 +80,7 @@ const Navbar = () => {
             </SheetTrigger>
             <SheetContent>
               <SheetHeader>
-                <SheetTitle className="font-bold my-4">HarryBlog</SheetTitle>
+                <SheetTitle className="font-bold my-4">Siam Mehraf</SheetTitle>
                 <SheetDescription>
                   <div className="flex flex-col gap-6">
                     <Link href="/"> Home</Link>
