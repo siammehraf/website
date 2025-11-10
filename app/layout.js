ee1fcdd4bc91199264d3e0b7c034b09f7c428356
siammehraf <siammehrafworks@gmail.com>
2025-11-10 23:57:31 +0600

    Speed Insight Fixed

--- app/layout.js
+++ app/layout.js
@@ -62,8 +62,8 @@ export default function RootLayout({ children }) {
         </ThemeProvider>
         <Footer />
         <BackToTopButton />
-        <Analytics />
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
