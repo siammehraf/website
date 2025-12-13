0fa171a08ba710bb942898b3ef2b5122685510d3
siammehraf <siammehrafworks@gmail.com>
2025-12-14 04:32:27 +0600

    Redirect Social Media Links Added

---
+++ middleware.js
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+
+/**
+ * CUSTOMIZE YOUR LINKS HERE
+ */
+const LINKS = {
+  facebook: 'https://facebook.com/siammehraf',
+  youtube: 'https://youtube.com/@siammehraf',
+  github: 'https://github.com/siammehraf',
+  twitter: 'https://twitter.com/siammehraf',
+  x: 'https://x.com/siammehraf',
+  'book-collections':
+    'https://siammehraf.notion.site/80ef552419e34f9ba2fde32dd8a05d4d?v=a662ba7ca0504887af4c724d62495451&pvs=74',
+};
+
+export function middleware(request) {
+  const slug = request.nextUrl.pathname.replace('/', '');
+
+  // If no match, do nothing
+  if (!LINKS[slug]) return;
+
+  // Fast + SEO-friendly redirect
+  return NextResponse.redirect(LINKS[slug], 308);
+}
