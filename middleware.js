4c0c6d6cd6d4070150b2d2ea00998b1036a4669c
siammehraf <siammehrafworks@gmail.com>
2025-12-17 00:18:43 +0600

    Some Social Media Links Added

--- middleware.js
+++ middleware.js
@@ -5,12 +5,13 @@ import { NextResponse } from 'next/server';
  */
 const LINKS = {
   facebook: 'https://facebook.com/siammehraf',
+  reels: 'https://www.facebook.com/siammehraf/reels/',
   youtube: 'https://youtube.com/@siammehraf',
+  shorts: 'https://youtube.com/@siammehrafshorts',
   github: 'https://github.com/siammehraf',
   twitter: 'https://twitter.com/siammehraf',
   x: 'https://x.com/siammehraf',
-  'book-collections':
-    'https://siammehraf.notion.site/80ef552419e34f9ba2fde32dd8a05d4d?v=a662ba7ca0504887af4c724d62495451&pvs=74',
+  library: 'https://siammehraf.notion.site/80ef552419e34f9ba2fde32dd8a05d4d?v=a662ba7ca0504887af4c724d62495451&pvs=74',
 };
 
 export function middleware(request) {
